c7dac878779b1a60d00f1ffca644a31b820a60df
brutalhonesty <sparky1010@gmail.com>
2013-10-24 16:57:24 -0700

    Initial commit, added description scraper

---
+++ README.md
@@ -0,0 +1,30 @@
+Update PHP Docs
+=========
+NodeJS script to recursively call the PHP site and get the description of each function and update the JSON file.
+
+Author
+------
+[Adam Schodde]
+
+Version
+-------
+
+0.0.1
+
+
+Run
+--------------
+
+```sh
+npm install
+node main.js
+```
+
+
+License
+-------
+
+[MIT]
+
+  [Adam Schodde]: http://github.com/brutalhonesty
+  [MIT]: http://www.tldrlegal.com/license/mit-license
---
+++ addDescription.js
@@ -0,0 +1,51 @@
+#!/usr/bin/node
+
+//http://stackoverflow.com/a/16066300/1612721
+var Stagger = function (data, stagger, fn, cb) {
+
+    var self        = this;
+    this.timerID    = 0;
+    this.data       = [].concat(data);
+    this.fn         = fn;
+    this.cb         = cb;
+    this.stagger    = stagger;
+    this.iteration  = 0;
+    this.store      = {};
+
+    this.start = function () {
+        (function __stagger() {
+            
+            self.fn(self.iteration, self.data[self.iteration], self.store);
+
+            self.iteration++;
+
+            if (self.iteration != self.data.length)
+                self.timerID = setTimeout(__stagger, self.stagger);
+            else
+                cb(self.store);
+
+        })();
+    };
+
+    this.stop = function () {
+        clearTimeout(self.timerID);
+        self.timerID = 0;
+    };
+};
+var jsdom = require('jsdom');
+var phpData = require('./data.php.json');
+var fs = require('fs');
+var jquery = fs.readFileSync("./jquery.js", "utf-8");
+var t = new Stagger(phpData, 4000, function (index, phpItem) {
+	var url = phpItem.url + '?setbeta=1&beta=1';
+	console.log(url);
+	jsdom.env({url: url, src: [jquery], done: function (errors, window) {
+		var $ = window.$;
+		var description = $('.refsect1.description').children('.para').text();
+		if(description !== '') {
+			description = description.trim();
+			phpItem.description = description;
+		}
+	}});
+});
+t.start();
